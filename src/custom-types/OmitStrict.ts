0628ab25feb321a212856de7db416dd302b472ed
bosung baek <teabs1125@gmail.com>
2024-02-07 02:27:03 +0900

    refactor: update '@see' tag

--- src/custom-types/OmitStrict.ts
+++ src/custom-types/OmitStrict.ts
@@ -1,5 +1,5 @@
 /**
  *  @description 조금 더 엄격하게 Omit을 사용하고 싶을 때 유용합니다.
- *  @see https://velog.io/@polaris6204/TypeScript-의-Omit-type-은-이상하다
+ *  @see {@link https://velog.io/@polaris6204/TypeScript-의-Omit-type-은-이상하다 Omit이 엄격하지 않은 이유}
  *  */
 export type OmitStrict<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
--- src/utils/string/formatValueToKoreanNumber.ts
+++ src/utils/string/formatValueToKoreanNumber.ts
@@ -1,6 +1,6 @@
 /**
  * @description 숫자 형태의 값을 받아와 언어(Ko-KR)와 관련된 표현이 있는 값을 반환합니다. 0에서는 값을 반환하지 않아요.
- * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toLocaleString
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toLocaleString MDN}
  * @example 천 단위마다 ','를 찍어줍니다.
  * ```
  * const foo = formatValueToKoreanNumber(1_234_567);
