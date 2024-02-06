6712d4fc94fc32bb74aa6cdc7fc38484673ebb7b
bosung baek <teabs1125@gmail.com>
2024-02-06 22:33:44 +0900

    feat: add 'formatValueKoreanNumber'

---
+++ src/utils/string/formatValueToKoreanNumber.ts
@@ -0,0 +1,16 @@
+/**
+ * @description 숫자 형태의 값을 받아와 언어(Ko-KR)와 관련된 표현이 있는 값을 반환합니다. 0에서는 값을 반환하지 않아요.
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toLocaleString
+ * @example 천 단위마다 ','를 찍어줍니다.
+ * ```
+ * const foo = formatValueToKoreanNumber(1_234_567);
+ * // 1,234,567
+ * ```
+ */
+export const formatValueToKoreanNumber = (value: string | number) => {
+  if (!value) return "";
+
+  const formattedNumber = Number(value).toLocaleString("ko-KR");
+
+  return `${formattedNumber}`;
+};
